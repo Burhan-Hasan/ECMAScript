66d9db0f4209d1d2044409d84000359e7e67b438
Burhan <burhan.hasan.dev@gmail.com>
2017-06-29 16:25:03 +0400

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var [name,surname,...otherDetails]=["Max","Payne","Dedective",35];
+
+console.log(`Name: ${name}`);
+console.log(`Surname: ${surname}`);
+console.log(`Details: ${otherDetails}`);
