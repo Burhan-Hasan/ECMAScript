68c079a9708dc8166e6fb47120e1201861e551bf
Burhan <burhan.hasan.dev@gmail.com>
2017-06-29 18:15:52 +0400

    changes

--- destructuring.js
+++ destructuring.js
@@ -1,7 +1,20 @@
 'use strict'
 
-var [name,surname,...otherDetails]=["Max","Payne","Dedective",35];
-
+var [name, surname, ...otherDetails] = ["Max", "Payne", "Dedective", 35];
+/*
 console.log(`Name: ${name}`);
 console.log(`Surname: ${surname}`);
 console.log(`Details: ${otherDetails}`);
+*/
+
+var kendoGrid = {
+    dataSource: [{ name: 'Max' }, { name: 'Alex' }],
+    table: {}
+}
+
+function Foo() {
+    let { dataSource } = kendoGrid;
+    console.log(dataSource);
+}
+
+Foo();
