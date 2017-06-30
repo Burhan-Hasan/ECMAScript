1138325857d3247f2ce087215c9475831b7750dc
Burhan <burhan.hasan.dev@gmail.com>
2017-06-30 20:16:06 +0400

    changes

--- destructuring-param.js
+++ destructuring-param.js
@@ -6,8 +6,24 @@ var user = {
     age: 35
 }
 
-function showInfo({name,surname:lastname,age})
-{
+function showInfo({ name, surname: lastname, age }) {
     console.log(`User with name ${name} ${lastname} has ${age} age.`);
 }
 showInfo(user);
+
+
+function getPointHome() {
+    return 5;
+}
+
+function getPointAway() {
+    return 18;
+}
+
+var obj = {
+    home:4,// getPointHome(),
+    away:6,// getPointAway(),
+    total:(()=>{console.log(this.home+this.away)})()
+}
+
+console.log(obj);
