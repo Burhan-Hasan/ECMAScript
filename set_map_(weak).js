e03dbd7f36dd678d1203d612662b350b4afa08ba
Burhan <burhan.hasan.dev@gmail.com>
2017-07-04 11:35:00 +0400

    changes

--- set_map_(weak).js
+++ set_map_(weak).js
@@ -6,8 +6,18 @@ let user = {
 
 let map = new Map();
 
-map.set(user,5)
+map.set(user, 5)
 
-var user2={};
-Object.assign(user2,user)
+var user2 = {};
+Object.assign(user2, user)
 map.get(user2);// undefined
+
+
+var usersActivity = new WeakMap();
+
+usersActivity.set(user, 50);
+
+console.log(usersActivity.get(user));//50
+user=null;
+console.log(usersActivity.get(user));//undefined
+
